25cd5832fd0c6c7cce7782c8e44a9f50f7c8ede9
sid-abc <69795417+sid-abc@users.noreply.github.com>
2021-09-14 01:35:27 +0530

    Time: 56 ms (15.78%), Space: 25.9 MB (55.53%) - LeetHub

---
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        for(int &i: nums)
+            i *= i;
+        
+        sort(nums.begin(), nums.end());
+        return nums;
+    }
+};

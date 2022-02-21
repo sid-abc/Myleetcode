50055979327c055991db3789251f53ce65b8010f
sid-abc <69795417+sid-abc@users.noreply.github.com>
2022-02-21 20:11:19 +0530

    Time: 9 ms (42.62%), Space: 11 MB (38.71%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        unordered_map<int, int> mp;
         int i = 0;
-        for(int x: nums) {
-            mp[x]++;
-            if(mp[x] < 3)
+        for(int x: nums)
+            if(i < 2 || x > nums[i - 2])
                 nums[i++] = x;
-        }
         return i;
     }
 };

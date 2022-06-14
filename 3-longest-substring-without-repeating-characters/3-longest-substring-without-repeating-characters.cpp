4a5e96f3fc7d0d0228c02588f7b9da0ce092ee0a
sid-abc <69795417+sid-abc@users.noreply.github.com>
2022-06-14 18:45:39 +0530

    Time: 25 ms (52.47%), Space: 8.4 MB (57.96%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,18 +1,36 @@
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//                    //char   index
+//         unordered_map<char, int> mp;
+//         int maxLen = -1e8, i, j = 0;
+        
+//         for(i=0; s[i]!='\0'; i++) {
+//             if(mp.count(s[i]) && mp[s[i]] >= j) {
+//                 maxLen = max(maxLen, i - j);
+//                 j = mp[s[i]] + 1;
+//             }
+//             mp[s[i]] = i;
+//         }
+//         maxLen = max(maxLen, i - j);
+        
+//         return maxLen;
+//     }
+// };
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-                   //char   index
         unordered_map<char, int> mp;
-        int maxLen = -1e8, i, j = 0;
+        int st = 0, end = 0, maxLen = 0;
         
-        for(i=0; s[i]!='\0'; i++) {
-            if(mp.count(s[i]) && mp[s[i]] >= j) {
-                maxLen = max(maxLen, i - j);
-                j = mp[s[i]] + 1;
-            }
-            mp[s[i]] = i;
+        while(s[end] != '\0') {
+            mp[s[end]]++;
+            while(mp[s[end]] > 1)
+                mp[s[st++]]--;
+            maxLen = max(maxLen, end - st + 1);
+            end++;
         }
-        maxLen = max(maxLen, i - j);
         
         return maxLen;
     }
